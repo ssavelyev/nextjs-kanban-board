0000dfbb3ab4c3ddb7c7f922bfcd6d7c2626dd94
Sergey Savelyev <itsgotrunn@gmail.com>
2026-03-23 10:40:41 +0300

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ export default function Home() {
     <main className="flex-1">
       {/* Hero section */}
       <section className="container mx-auto px-4 py-32">
-        <div>
-          <h1 className="text-black">Kanban board</h1>
-          <p className="text-muted-foreground">A complete kanban board with mongoDB database integration</p>
+        <div className="mx-auto max-v-4xl text-center">
+          <h1 className="text-black mb-6 text-6xl font-bold">Kanban board</h1>
+          <p className="text-muted-foreground mb-10 text-xl">A complete kanban board with mongoDB database integration</p>
+          <div className="flex flex-col items-center gap-4">
+            <button>Start for free</button>
+            <p>Free forever</p>
+          </div>
         </div>
       </section>
     </main>
