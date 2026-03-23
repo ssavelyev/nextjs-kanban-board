297ee6d5597e78fe7fdbfb4e17c74172b6c6fdce
Sergey Savelyev <itsgotrunn@gmail.com>
2026-03-23 10:31:28 +0300

    added color palette

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div className="flex min-h-screen flex-col bg-white"></div>
+  return <div className="flex min-h-screen flex-col bg-white">
+    <main className="flex-1">
+      {/* Hero section */}
+      <section className="container mx-auto px-4 py-32">
+        <div>
+          <h1 className="text-black">Kanban board</h1>
+          <p className="text-muted-foreground">A complete kanban board with mongoDB database integration</p>
+        </div>
+      </section>
+    </main>
+  </div>
 }
