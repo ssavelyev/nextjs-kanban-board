f5c2189c37c95e65e18d20d665793bf97dc0b39b
Sergey Savelyev <itsgotrunn@gmail.com>
2026-03-23 17:53:44 +0300

    added features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,14 @@
+import Features from "@/components/features";
+import Hero from "@/components/hero";
 import Tabs from "@/components/tabs";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <main className="flex-1">
-        {/* Hero section */}
-        <section className="container mx-auto px-4 py-32">
-          <div className="mx-auto max-v-4xl text-center">
-            <h1 className="text-black mb-6 text-6xl font-bold">Kanban board</h1>
-            <p className="text-muted-foreground mb-10 text-xl">
-              A complete kanban board with mongoDB database integration
-            </p>
-            <div className="flex flex-col items-center gap-4">
-              <Link href="/sign-up">
-                <Button size="lg" className="h-12 px-8 text-lg font-medium">
-                  Start
-                </Button>
-              </Link>
-              <p className="text-sm text-muted-foreground">Free forever</p>
-            </div>
-          </div>
-        </section>
-        {/* Tabs section */}
-    <Tabs />
+        <Hero/>
+        <Tabs />
+        <Features />
       </main>
     </div>
   );
---
+++ components/hero.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <section className="container mx-auto px-4 py-32">
+      <div className="mx-auto max-v-4xl text-center">
+        <h1 className="text-black mb-6 text-6xl font-bold">Kanban board</h1>
+        <p className="text-muted-foreground mb-10 text-xl">
+          A complete kanban board with mongoDB database integration
+        </p>
+        <div className="flex flex-col items-center gap-4">
+          <Link href="/sign-up">
+            <Button size="lg" className="h-12 px-8 text-lg font-medium">
+              Start
+            </Button>
+          </Link>
+          <p className="text-sm text-muted-foreground">Free forever</p>
+        </div>
+      </div>
+    </section>
+  );
+}
