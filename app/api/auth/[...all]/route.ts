03ae322e0e1dcbe7fc3b13d0b3ac67321e0704a2
Sergey Savelyev <itsgotrunn@gmail.com>
2026-03-31 13:24:28 +0300

    db updated

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,26 +1,4 @@
-import { pool } from "@/lib/auth/auth";
-import { NextResponse } from "next/server";
+import { auth } from "@/lib/auth/auth";
+import { toNextJsHandler } from "better-auth/next-js";
 
-export async function POST(req: Request) {
-  try {
-    const { name, email, password } = await req.json();
-
-    if (!name || !email || !password) {
-      return NextResponse.json({ error: "Missing fields" }, { status: 400 });
-    }
-
-    const result = await pool.query(
-      `
-      INSERT INTO test_users (name, email, password)
-      VALUES ($1, $2, $3)
-      RETURNING id, name, email
-      `,
-      [name, email, password],
-    );
-
-    return NextResponse.json(result.rows[0]);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Server error" }, { status: 500 });
-  }
-}
+export const { GET, POST } = toNextJsHandler(auth);
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,5 +1,13 @@
-import { Pool } from "pg";
+import { betterAuth } from "better-auth";
+import { pool } from "../db";
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL!,
+  database: pool,
+  emailAndPassword: {
+    enabled: true,
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+  },
 });
